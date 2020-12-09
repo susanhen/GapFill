8a6cdbb2816ecf5a47bce775f328307abd985b0f
Susanne Støle-Hentschel <susanhen@math.uio.no>
2020-12-09 22:06:47 +0100

    include data_chopping

--- deconvolution.py
+++ deconvolution.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 
 import numpy as np
-from scipy.linalg import toeplitz, circulant
+from scipy.linalg import toeplitz
 
 def solve_with_options_reduced(A_reduced, b, method):
     def square_A_reduced(A_reduced):
@@ -34,8 +34,41 @@ def select_indices(w, w1, w2, w0):
         
     return selected_indices, refill_selected_indices
     
+def grid2spectral(grid):
+    dx = abs(grid[-1]-grid[-2])    
+    N = len(grid)
+    kmin = -np.pi/dx
+    dk = 2*np.pi/(dx*N)
+    k = kmin + dk*np.arange(0,N)
+    return k
     
-class Deconvolution:
+def spectral2grid(k):   
+    dk = abs(k[-1]-k[-2])
+    N = len(k)
+    dx = 2*np.pi/(dk*N)
+    x = np.arange(0,N)*dx
+    return x
+
+def suggest_band_limitation(t, y, mask, plot_it=True):
+    N = len(t)
+    w = grid2spectral(t)
+    fft_y_mask = np.fft.fftshift(np.fft.fft(y*mask))
+    Sp = np.abs(fft_y_mask[N//2:])**2
+    ind_max = np.argmax(Sp)
+    E_tot = np.sum(Sp)
+    ind1 = (N//2 + 1 )+ np.argwhere(Sp[1:] > 0.001*E_tot)[0]
+    ind2 = (N//2 + ind_max) + np.argwhere(Sp[ind_max:] > 0.001*E_tot)[-1]
+    if plot_it:
+        import pylab as plt
+        plt.plot(w[N//2:], Sp/np.max(Sp), color='b', linestyle='-', marker='+')
+        plt.plot([w[ind1], w[ind1]], [0,1.1], 'k:')  
+        plt.plot([w[ind2], w[ind2]], [0,1.1], 'k:')           
+        plt.xlabel(r'$\omega$')
+        plt.ylabel(r'$S/S_p(\omega)$')
+        plt.show()
+    return w[ind1], w[ind2]
+    
+class DeconvolutionSetup:
     """
     A class to perform the deconvolution as interpolation
 
@@ -43,8 +76,6 @@ class Deconvolution:
 
     Attributes
     ----------
-    t : array
-        time array for measurements (assumed to be uniform)
     N : int
         length of measurement and time array
     N_half : int
@@ -70,27 +101,25 @@ class Deconvolution:
         interpolates the missing points by reducing the matrix
     """
     
-    def __init__(self, t, w1, w2, w0=False, method='solve'):   
+    def __init__(self, w, w1, w2, w0=False, method='solve'):   
     
         """
         Parameters
         ----------
-        t : array
-            time array for measurements (assumed to be uniform)
+        w : array
+            frequency vector for the coefficients of the data to be deconvolved
         w1 : float
             lower limit on the frequency line for the bandwidth limitation
         w2 : float
-            upper limit on the frequency line for the bandwidth limitation            
+            upper limit on the frequency line for the bandwidth limitation  
+        w0 : bool, optional
+            switch if zeroth frequency component should be solved for (default is False)
         method : string, optional
             The method for interpolating (default is 'solve')
         """
-        self.t = t
-        self.N = len(t)
-        self.N_half = int(0.5*self.N)
-        dt = abs(t[-1]-t[-2])    
-        wmin = -np.pi/dt
-        dw = 2*np.pi/(dt*self.N)        
-        self.w = wmin + dw*np.arange(0,self.N)
+        self.w = w
+        self.N = len(w)
+        self.N_half = self.N//2
         if w2>self.w[-1]:
             w2 = self.w[-1]
             print("w2 was set to {0:.4f}, input value was too large".format(self.w[-1]))
@@ -158,6 +187,12 @@ class Deconvolution:
     
     def direct_interpolate(self, eta_shad, illu, plot_spec=False, replace_all=False):    
         """
+        Conducts interpolation by deconvolution directly (without 
+        manipulating the convolution matrix). The method is only
+        successful when the gaps are narrow.
+        
+        ...
+        
         Parameters
         ----------
         eta_shad : array
@@ -188,6 +223,13 @@ class Deconvolution:
         
     def interpolate(self, eta_shad, illu, plot_spec=False, replace_all=False):  
         """
+        Standard method for interpolation by deconvolution. The matrix is reduced
+        by assuming that only the coefficients corresponding to the frequency
+        range [w1,w2] to be non-zero. Further, the conjugate symmetry of the complex
+        coefficients is exploited to further reduce the problem.
+        
+        ...
+        
         Parameters
         ----------
         eta_shad : array
@@ -237,6 +279,11 @@ class Deconvolution:
 
     def interpolate_non_symmetric(self, eta_shad, illu, plot_spec=False, replace_all=False):  
         """
+        The method is similar to interpolate but does not exploit the conjugate symmetry.
+        The results of both methods should normally be identical but interpolate should be faster.
+        
+        ...
+        
         Parameters
         ----------
         eta_shad : array
@@ -276,6 +323,143 @@ class Deconvolution:
         return eta_out
 
 
+
+
+class DataOrganizer:
+    '''
+    Class to organize data for deconvolution.
+    The deconvolution for large datasets should be preformed on chunks.
+    To avoid problems at the boundary of the chunks, the deconvolution
+    is performed twice so that all gaps can be replaced by solutions 
+    that are not close to a boundary.
+
+    Attributes
+    ----------  
+    t              : array
+                    Timeline for measurements
+    y              : array
+                    Measurements  along the timeline
+    m              : array
+                    Mask the gaps in y
+    N              : int
+                    Number of points
+    N_per interval : int
+                    Number of points per interval                    
+    start_points1  : array 
+                    Start points of intervals
+    end_points1    : array 
+                    End points of intervals
+    start_points2  : array 
+                    Start points of intervals shifted relative to the first set    
+    end_points2    : array 
+                    End points of intervals shifted relative to the first set    
+    start_replace  : array 
+                    Start points of intervals for replacing set1 by set2
+    end_preplace   : array 
+                    End points of intervals for replacing set1 by set2
+    '''
+    def __init__(self, t, y, m, N_intervals):
+        self.N_intervals = N_intervals
+        interval_mod = np.mod(len(y), N_intervals)
+        self.stop_index = len(y) # index for undoing increase in size due to intervals        
+
+        if interval_mod != 0:
+            interval_mod = np.mod(len(y), N_intervals-1)
+            self.N_per_interval = (len(y) - interval_mod)//(N_intervals-1)
+            self.y = np.block([y, np.zeros(self.N_per_interval)])
+            self.m = np.block([m, np.zeros(self.N_per_interval)])
+            dt = t[1] - t[0]
+            self.t = np.arange(0, len(y)) * dt
+        else:
+            self.N_per_interval = len(y)//N_intervals
+            self.y = y
+            self.m = m
+            self.t = t
+        self.N = len(self.y)
+        self.start_points1 = np.arange(0, self.N_intervals)*self.N_per_interval
+        self.end_points1 = np.block([self.start_points1[1:], self.N])
+        if N_intervals>1:
+            self.start_points2 = self.N_per_interval//2 + np.arange(0, self.N_intervals-1)*self.N_per_interval
+            self.end_points2 = np.block([self.start_points2[1:], self.start_points2[-1]+self.N_per_interval])
+            self.start_replace = self.start_points2 + self.N_per_interval//4
+            self.end_replace = self.end_points2 - self.N_per_interval//4
+        else:
+            self.start_points2 = []
+        self.w_interval = grid2spectral(t[:self.N_per_interval])
+        self.w1, self.w2 = suggest_band_limitation(t, y, m)
+        
+    def deconvolve(self, w0=False, method='solve', plot_spec=False, replace_all=False):
+        # deconvolve first set of intervals
+        t_local = self.t[0: self.N_per_interval]
+        w_local = grid2spectral(t_local)        
+        y_dec = np.zeros(self.N)
+        dec_setup = DeconvolutionSetup(w_local, self.w1, self.w2, w0, method)
+        ind = self.N_per_interval//4
+        
+        for i in range(0, len(self.start_points1)):
+            y_local = self.y[self.start_points1[i]: self.end_points1[i]]
+            m_local = self.m[self.start_points1[i]: self.end_points1[i]]
+
+            #TODO: check below
+            #y_local -= np.mean(y_local[np.argwhere(m_local==1)])#CHECKTHIS!!!
+            y_dec[self.start_points1[i]:self.end_points1[i]] = dec_setup.interpolate(y_local, m_local, plot_spec, replace_all)
+    
+        for i in range(0,len(self.start_points2)):
+            y_local = self.y[self.start_points2[i]: self.end_points2[i]]
+            m_local = self.m[self.start_points2[i]: self.end_points2[i]]
+
+            #TODO: check below
+            #y_local -= np.mean(y_local[np.argwhere(m_local==1)])#CHECKTHIS!!!
+            dec_hold = dec_setup.interpolate(y_local, m_local, plot_spec, replace_all)
+
+            N_replace = self.end_replace[i]-self.start_replace[i]
+            y_dec[self.start_replace[i]:self.end_replace[i]] = dec_hold[ind:ind+N_replace]
+        
+        return y_dec
+    
+        
+    def plot_data_all(self, fn='test_plot', save=False):
+        self.plot_data(0, self.N, fn, save)        
+    
+    def plot_data(self, start_index, stop_index, fn='test_plot', save=False):
+        t_sub = self.t[start_index:stop_index]
+        import pylab as plt
+        plt.figure(figsize=(10,4))
+        plt.plot(t_sub, self.y[start_index:stop_index], '-', color='navy', label='measured')
+        #plt.plot(t_sub, data_spl[start_point:end_point], '--', color='orchid', label=r'$\mathrm{interpol}$')
+        #plt.plot(t_sub, data_dec[start_point:end_point], '-.', color='darkorange', label=r'$\mathrm{deconvolved}$')
+        plt.plot(t_sub, self.m[start_index:stop_index], 'k:', label='error marker')
+        plt.ylabel(r'$\eta~[m]$')
+        plt.xlabel(r'$t~[s]$')
+        plt.legend(bbox_to_anchor=(0., 1.02, 1., .102), loc=3, ncol=4, mode="expand", borderaxespad=0.)   
+        if save:
+            plt.savefig('{0:s}.pdf'.format(fn), bbox_inches='tight')
+        plt.show()
+        
+    def plot_spec_limits(self):
+        import pylab as plt
+        import matplotlib as mpl
+        #from matplotlib.texmanager import TexManager
+        #mpl.rcParams['text.usetex'] = True  
+        plt.figure(figsize=(5,5))
+        fft_y_m = np.fft.fftshift(np.fft.fft(self.y*self.m))
+        Sp = np.abs(fft_y_m[self.N//2:])**2
+        Sp_max = np.max(Sp)
+        w = grid2spectral(self.t)
+        plt.plot(w[self.N//2:], Sp/Sp_max, label=r'$\hat{\eta}_{\mathrm{measured and masked}}$', color='b', linestyle='-', marker='+')
+        plt.plot([self.w1, self.w1], [0,1.1], 'k:')  
+        plt.plot([self.w2, self.w2], [0,1.1], 'k:')           
+        plt.legend()
+        plt.xlabel(r'$\omega$')
+        plt.ylabel(r'$S/S_p(f)$')
+        plt.show()  
+        
+    def reset_w1(self, w1):
+        self.w1 = w1  
+
+    def reset_w2(self, w2):
+        self.w2 = w2
+
         
 if __name__ == '__main__':
     import pylab as plt
@@ -305,9 +489,13 @@ if __name__ == '__main__':
     mean_gap_length = 8
     illu = construct_mask(N, N_gaps, mean_gap_length)    
 
+    dt = abs(t[-1]-t[-2])    
+    wmin = -np.pi/dt
+    dw = 2*np.pi/(dt*N)        
+    w = wmin + dw*np.arange(0, N)
     w1 = 0.02
     w2 = 0.16
-    Dec = Deconvolution(t, w1, w2)
+    Dec = DeconvolutionSetup(w, w1, w2)
     
     
     eta_dec = Dec.interpolate(eta*illu, illu, replace_all=True, plot_spec=True)
--- test.py
+++ test.py
@@ -1,6 +1,6 @@
 import unittest
 import numpy as np
-from deconvolution import Deconvolution
+from deconvolution import DeconvolutionSetup, DataOrganizer
 
 
 class TestDeconvolution(unittest.TestCase):
@@ -8,16 +8,20 @@ class TestDeconvolution(unittest.TestCase):
     def setUp(self):
         self.N = 100
         self.t = np.linspace(0, 500, self.N)
+        dt = abs(self.t[-1]-self.t[-2])    
+        wmin = -np.pi/dt
+        dw = 2*np.pi/(dt*self.N)        
+        self.w = wmin + dw*np.arange(0, self.N)
         self.w1 = 0.03
         self.w2 = 0.2
-        self.dec = Deconvolution(self.t, self.w1, self.w2)
+        self.dec_set = DeconvolutionSetup(self.w, self.w1, self.w2)
         
     def test_convolution(self):
         a = np.sign(np.sin(self.t*10))
         b = np.cos(self.t)
         fft_a = np.fft.fftshift(np.fft.fft(a))/self.N
         fft_b = np.fft.fftshift(np.fft.fft(b))/self.N
-        A = self.dec.build_convolution_matrix(a)
+        A = self.dec_set.build_convolution_matrix(a)
         matrix_convolution = np.flipud(np.dot(A, np.flipud(fft_b)))
         
         N_half = int(0.5*self.N)
@@ -33,7 +37,7 @@ class TestDeconvolution(unittest.TestCase):
         RMS_expected = 0.008411               
         b = np.cos(0.05*self.t) + 0.2*np.cos(0.07*self.t+0.3*np.pi)   
         b -= np.mean(b)     
-        b_dec = self.dec.interpolate(b, a)
+        b_dec = self.dec_set.interpolate(b, a)
         diff = np.abs(b[ind1:ind2] - b_dec[ind1:ind2])**2
         RMS = np.round(np.sqrt(np.mean(diff)), 6)
         self.assertEqual(RMS, RMS_expected)    
@@ -46,10 +50,63 @@ class TestDeconvolution(unittest.TestCase):
         RMS_expected = 0.008411
         b = np.cos(0.05*self.t) + 0.2*np.cos(0.07*self.t+0.3*np.pi)   
         b -= np.mean(b)     
-        b_dec = self.dec.interpolate_non_symmetric(b, a)
+        b_dec = self.dec_set.interpolate_non_symmetric(b, a)
         diff = np.abs(b[ind1:ind2] - b_dec[ind1:ind2])**2
         RMS = np.round(np.sqrt(np.mean(diff)), 6)
         self.assertEqual(RMS, RMS_expected)
+
+
+class TestDataOrganizer(unittest.TestCase): 
+ 
+    def setUp(self):  
+        self.N = 200
+        self.t = np.linspace(0, 1000, self.N)
+        self.y = np.cos(0.05*self.t) + 0.2*np.cos(0.07*self.t+0.3*np.pi)   
+        self.y -= np.mean(self.y)
+        self.m = np.ones(self.N)
+        ind1 = int(0.22*self.N)
+        ind2 = int(0.27*self.N)
+        self.m[ind1:ind2] = 0
+        ind1 = int(0.47*self.N)
+        ind2 = int(0.52*self.N)
+        self.m[ind1:ind2] = 0
+        self.N_intervals = 2
+        self.data_organizer = DataOrganizer(self.t, self.y*self.m, self.m, self.N_intervals)    
+        
+    def test_data_chopping(self):
+        N_int = self.data_organizer.N_intervals
+        N_per_int = self.data_organizer.N_per_interval
+        start1_compare = 0
+        start2_compare = N_per_int//2
+        for i in range(0, N_int-1):            
+            start1 = self.data_organizer.start_points1[i]
+            self.assertEqual(start1, start1_compare)
+            start1_compare += N_per_int
+            end1 = self.data_organizer.end_points1[i]
+            self.assertEqual(start1_compare, end1)
+            
+            start2 = self.data_organizer.start_points2[i]            
+            self.assertEqual(start2, start2_compare)
+            start2_compare += N_per_int
+            end2 = self.data_organizer.end_points2[i]
+            self.assertEqual(start2_compare, end2)            
+                    
+        start1 = self.data_organizer.start_points1[-1]
+        self.assertEqual(start1, start1_compare)
+        start1_compare += N_per_int
+        end1 = self.data_organizer.end_points1[-1]
+        self.assertEqual(start1_compare, end1)
+        self.data_organizer.plot_spec_limits() 
+        
+    def test_deconvolution(self):
+        dec = self.data_organizer.deconvolve()
+        import pylab as plt
+        plt.plot(dec)
+        plt.plot(self.y)
+        plt.plot(self.m)
+        plt.show()
+        
+            
        
         
 
